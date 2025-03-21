7286ea76d4aa508ad80624c2f14de9641bbc7044
Sagar Sable <129820203+sagarsable71190@users.noreply.github.com>
2025-03-21 23:24:41 +0530

    Update UiPathDeploy.ps1

--- Scripts/UiPathDeploy.ps1
+++ Scripts/UiPathDeploy.ps1
@@ -81,8 +81,49 @@ function WriteLog
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $debugLog = "$scriptPath\orchestrator-package-deploy.log"
 
-#Verifying UiPath CLI folder
-$uipathCLI = "$scriptPath\uipathcli\23.10.8753.32995\tools\uipcli.exe"
+#Validate provided cli folder (if any)
+if($uipathCliFilePath -ne ""){
+    $uipathCLI = "$uipathCliFilePath"
+    if (-not(Test-Path -Path $uipathCLI -PathType Leaf)) {
+        WriteLog "UiPath cli file path provided does not exist in the provided path $uipathCliFilePath.`r`nDo not provide uipathCliFilePath paramter if you want the script to auto download the cli from UiPath Public feed"
+        exit 1
+    }
+}else{
+    #Verifying UiPath CLI installation
+    if($SpecificCLIVersion -ne ""){
+        $cliVersion = $SpecificCLIVersion;
+    }
+    else{
+        $cliVersion = "23.10.8753.32995"; #CLI Version (Script was tested on this latest version at the time)
+    }
+
+    $uipathCLI = "$scriptPath\uipathcli\$cliVersion\tools\uipcli.exe"
+    if (-not(Test-Path -Path $uipathCLI -PathType Leaf)) {
+        WriteLog "UiPath CLI does not exist in this folder. Attempting to download it..."
+        try {
+            if (-not(Test-Path -Path "$scriptPath\uipathcli\$cliVersion" -PathType Leaf)){
+                New-Item -Path "$scriptPath\uipathcli\$cliVersion" -ItemType "directory" -Force | Out-Null
+            }
+            #Download UiPath CLI
+            #Invoke-WebRequest "https://www.myget.org/F/uipath-dev/api/v2/package/UiPath.CLI/$cliVersion" -OutFile "$scriptPath\\uipathcli\\$cliVersion\\cli.zip";
+            Invoke-WebRequest "https://uipath.pkgs.visualstudio.com/Public.Feeds/_apis/packaging/feeds/1c781268-d43d-45ab-9dfc-0151a1c740b7/nuget/packages/UiPath.CLI.Windows/versions/$cliVersion/content" -OutFile "$scriptPath\\uipathcli\\$cliVersion\\cli.zip";
+            Expand-Archive -LiteralPath "$scriptPath\\uipathcli\\$cliVersion\\cli.zip" -DestinationPath "$scriptPath\\uipathcli\\$cliVersion";
+            WriteLog "UiPath CLI is downloaded and extracted in folder $scriptPath\uipathcli\\$cliVersion"
+            if (-not(Test-Path -Path $uipathCLI -PathType Leaf)) {
+                WriteLog "Unable to locate uipath cli after it is downloaded."
+                exit 1
+            }
+        }
+        catch {
+            WriteLog ("Error Occured : " + $_.Exception.Message) -err $_.Exception
+            exit 1
+        }
+        
+    }
+}
+WriteLog "-----------------------------------------------------------------------------"
+WriteLog "uipcli location :   $uipathCLI"
+#END Verifying UiPath CLI installation
 
 WriteLog "-----------------------------------------------------------------------------"
 WriteLog "uipcli location :   $uipathCLI"
